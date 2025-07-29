6fd8a81c3a79a119438269cc826e0c78a6da1e40
Henry <cootshk@duck.com>
2025-07-29 11:37:47 -0800

    prettify tables

---
+++ src/globals.lua
@@ -0,0 +1,14 @@
+---@diagnostic disable: duplicate-set-field
+local write = require("pl.pretty").write
+_G.log = function(...)
+    if not ENABLE_LOGS then
+        return
+    end
+    local args = {...}
+    for i, v in ipairs(args) do
+        if type(v) == "table" then
+        args[i] = write(v)
+        end
+    end
+    print(table.concat(args, " "))
+end
