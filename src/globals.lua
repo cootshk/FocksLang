7fde037405c94c33e50e89d7dbf7e466374728fb
Henry <cootshk@duck.com>
2025-07-30 00:17:15 -0800

    start of real compiling

--- src/globals.lua
+++ src/globals.lua
@@ -1,5 +1,24 @@
 ---@diagnostic disable: duplicate-set-field
-local write = require("pl.pretty").write
+
+-- Waiting on real lux compiling (luastatic?) support
+---@type function
+local write
+local success, _ = pcall(function()
+	write = require("pl.pretty").write
+end)
+if not success then
+	write = function(tbl)
+		local i = 1
+		---@type string
+		local output = ""
+		for k, v in pairs(tbl) do
+			---@type string
+			output = output .. "\n" .. i .. ". " .. k .. " = " .. v
+		end
+		return output:sub(2)
+	end
+end
+
 _G.log = function(...)
 	if not ENABLE_LOGS then
 		return
